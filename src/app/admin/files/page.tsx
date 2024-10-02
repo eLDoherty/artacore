ee2597337e71b3039569d6752d16b086a27dcdb9
eLDoherty <leonardodoherty@gmail.com>
2024-10-02 22:17:17 +0700

    first init

---
+++ src/app/admin/files/page.tsx
@@ -0,0 +1,10 @@
+// src/admin/posts/page.tsx
+
+export default function FilePage() {
+    return (
+        <div>
+            <h2>Manage File Manager</h2>
+            <p>Here you can create, edit, or delete posts.</p>
+        </div>
+    );
+}
---
+++ src/app/admin/page.tsx
@@ -0,0 +1,10 @@
+// src/admin/page.tsx
+
+export default function AdminPage() {
+    return (
+        <div>
+            <h2>Admin Dashboard</h2>
+            <p>Welcome to the admin dashboard!</p>
+        </div>
+    );
+}
