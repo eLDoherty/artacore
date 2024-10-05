173f3fdf430eeff93ceb5f2f83e5f538ac9ef733
eLDoherty <leonardodoherty@gmail.com>
2024-10-05 12:57:04 +0700

    Update env

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,11 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'artacore',
-});
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
 
 export async function insert(table: string, data: Record<string, any>) {
     const keys = Object.keys(data).join(', ');
